81200e186ff48fd4f3106531b6b38af43f5782e0
unknown <jared.fraryq@gmail.com>
2025-11-06 22:17:24 -0500

    W01 Prove: Developer-C# Prep1 - 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage?");
+        int percent = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed, Great Job!");
+        }
+        else
+        {
+            Console.WriteLine("Try again Bub");
+        }
+         
+        
     }
 }
