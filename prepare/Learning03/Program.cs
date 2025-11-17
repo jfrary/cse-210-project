089c68be48300addcd117d876f1ec305206907fa
unknown <jared.fraryq@gmail.com>
2025-11-17 10:19:17 -0500

    Learning 03 Fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,7 +3,26 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+  {
+
+Fraction p1 = new Fraction();
+
+Fraction p2 = new Fraction(5);
+
+Fraction p3 = new Fraction(6, 7);
+
+Fraction p4 = new Fraction(3, 4);
+
+
+
+Console.WriteLine(p1.GetFractionString());
+Console.WriteLine(p1.GetDecimalValue());
+Console.WriteLine(p2.GetFractionString());
+Console.WriteLine(p2.GetDecimalValue());
+Console.WriteLine(p3.GetFractionString());
+Console.WriteLine(p3.GetDecimalValue());
+Console.WriteLine(p4.GetFractionString());
+Console.WriteLine(p4.GetDecimalValue());
     }
 }
+
